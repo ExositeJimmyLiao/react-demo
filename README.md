113561e04c0f506897abaef846c05a9971e332a6
Jimmy Liao <jimmyliao@exosite.com>
2017-02-18 09:28:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React.js Demo
+How to run this repo.   
+1. `git clone https://github.com/ExositeJimmyLiao/react-demo`   
+2. `cd react-demo`   
+3. `npm install`   
+4. `npm start`
+5. See your browser
---
+++ src/index.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const deviceJson = [
+  {
+    id: '000001',
+    name: '冷氣機1',
+  },
+  {
+    id: '000002',
+    name: '冷氣機2',
+  },
+  {
+    id: '000003',
+    name: '冷氣機3',
+  }, 
+];
+
+const Device = props => (
+  <div>
+    <h3>Device ID: {props.id}</h3>
+    <p>Device Name: {props.name}</p>
+  </div>
+);
+
+const DeviceList = (props) => {
+  const list = props.list.map((item) => {
+    return <Device ...item key={item.id} />;
+  });
+  return (
+      <div>
+        {list}
+      </div>
+  );
+}
+
+class DeviceAdd extends Component {
+
+  addOnClick = (event) => {
+    event.preventDefault();
+    const device = {
+      id: this.idDom.value,
+      name: this.nameDom.value,
+    };
+    this.props.onChange(device);
+    this.idDom.value = '';
+    this.nameDom.value = '';
+  }
+ 
+  render() {
+    return (
+      <form>
+        <h3>New Device</h3>
+        <p>Device ID: <input type="text" ref={function(dom) {
+          this.idDom = dom;
+        }.bind(this)}/></p>
+        <p>Device Name: <input type="text" ref={dom => this.nameDom = dom}/></p>
+        <button onClick={this.addOnClick}>
+          <h3>Add</h3>
+        </button>
+      </form>
+    );
+  }
+}
+
+class DeviceApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: deviceJson
+    };
+  }
+
+  onAddDevice = (device) => {
+    this.setState({
+      list: [...this.state.list, device]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <DeviceAdd onChange={this.onAddDevice}/>
+        <DeviceList list={this.state.list}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <DeviceApp />,
+  document.getElementById('root'),
+);
